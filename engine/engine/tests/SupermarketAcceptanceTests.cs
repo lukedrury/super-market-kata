eb96cc857b6a72f8f9e7c3c5f4d8dcd783700f0b
Luke Drury <dakmor00@gmail.com>
2014-11-01 23:39:50 +0000

    Red: failing test for empty basket

--- engine/engine/tests/SupermarketAcceptanceTests.cs
+++ engine/engine/tests/SupermarketAcceptanceTests.cs
@@ -5,5 +5,28 @@ namespace engine.tests
     [TestFixture]
     public class SupermarketAcceptanceTests
     {
+        [Test]
+        public void EmptyBasket()
+        {
+            var till = new Till();
+            var basket = new Basket();
+
+            var total = till.CalculatePrice(basket);
+
+            var expected = 0;
+            Assert.That(total, Is.EqualTo(expected));
+        }
+    }
+
+    public class Basket
+    {
+    }
+
+    public class Till
+    {
+        public int CalculatePrice(Basket basket)
+        {
+            return -1;
+        }
     }
 }
