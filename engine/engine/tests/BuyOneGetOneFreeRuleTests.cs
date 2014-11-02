0502dac6c5e55c545cf4f13c7c5b03ab15419d62
Luke Drury <dakmor00@gmail.com>
2014-11-02 00:52:42 +0000

    Passing test for BOGOF applied to basket with multiple unrelated items

--- engine/engine/tests/BuyOneGetOneFreeRuleTests.cs
+++ engine/engine/tests/BuyOneGetOneFreeRuleTests.cs
@@ -32,6 +32,19 @@ namespace engine.tests
             Assert.That(returnedBasket, Is.EquivalentTo(expectedBasket));
         }
 
+        [Test]
+        public void BasketWithMultipleUnrelatedItems()
+        {
+            m_InputBasket.Add("apple", 1, 3);
+            var rule = new BuyOneGetOneFreeRule("item");
+
+            var returnedBasket = rule.Apply(m_InputBasket);
+
+            var expectedBasket = new Basket();
+            expectedBasket.Add("apple", 1, 3);
+            Assert.That(returnedBasket, Is.EquivalentTo(expectedBasket));
+        }
+
         [SetUp]
         public void Setup()
         {
