e1db616d44e857f78101c4bcb91abe0c2af649b6
Luke Drury <dakmor00@gmail.com>
2014-11-02 01:21:52 +0000

    Green: implement logic for BOGOF rule.

--- engine/engine/tests/SupermarketAcceptanceTests.cs
+++ engine/engine/tests/SupermarketAcceptanceTests.cs
@@ -65,13 +65,45 @@ namespace engine.tests
 
     public class BuyOneGetOneFreeRule : Rule
     {
+        private readonly string m_Item;
+
         public BuyOneGetOneFreeRule(string item)
         {
+            m_Item = item;
         }
 
         public override Basket Apply(Basket basket)
         {
-            return basket;
+            var updatedBasket = new Basket();
+
+            BasketItem previousApplicableItem = null;
+            foreach (var item in basket)
+            {
+                if (item.Item.Equals(m_Item))
+                {
+                    if (previousApplicableItem == null)
+                    {
+                        previousApplicableItem = item;
+                    }
+                    else
+                    {
+                        updatedBasket.Add(new BasketItem(previousApplicableItem.Item, previousApplicableItem.UnitPrice, true), 2);
+                        updatedBasket.Add(new BasketItem(string.Format("{0}:bogof", previousApplicableItem.Item), -1, true));
+                        previousApplicableItem = null;
+                    }
+                }
+                else
+                {
+                    updatedBasket.Add(item);
+                }
+            }
+
+            if (previousApplicableItem != null)
+            {
+                updatedBasket.Add(previousApplicableItem);
+            }
+
+            return updatedBasket;
         }
     }
 
