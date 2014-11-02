efc52c5911cee3dbd0cd0309335c217ad0f771d3
Luke Drury <lukecdrury@gmail.com>
2014-11-02 16:58:41 +0000

    Passing test for basket with unrelated item in.

--- supermarketkata/engine/tests/PercentageDiscountTests.cs
+++ supermarketkata/engine/tests/PercentageDiscountTests.cs
@@ -20,6 +20,19 @@ namespace engine.tests
             Assert.That(returnedBasket, Is.EquivalentTo(expectedBasket));
         }
 
+        [Test]
+        public void BasketWithOneUnrelatedItem()
+        {
+            var rule = new PercentageDiscount("discountedItem", 100);
+            m_InputBasket.Add("apple", 1);
+
+            var returnedBasket = rule.Apply(m_InputBasket);
+
+            var expectedBasket = new Basket();
+            expectedBasket.Add("apple", 1);
+            Assert.That(returnedBasket, Is.EquivalentTo(expectedBasket));
+        }
+
         [SetUp]
         public void Setup()
         {
