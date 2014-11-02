246340d2c1e2be6c3ee7ae7accd1c4e687112ecb
Luke Drury <lukecdrury@gmail.com>
2014-11-02 17:47:07 +0000

    R# move class to own file.

--- supermarketkata/engine/tests/PercentageDiscountTests.cs
+++ supermarketkata/engine/tests/PercentageDiscountTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using engine.core;
+﻿using engine.core;
 using engine.items;
 using engine.rules;
 using NUnit.Framework;
@@ -97,36 +96,6 @@ namespace engine.tests
         public void Setup()
         {
             m_InputBasket = new Basket();
-        }
-    }
-
-    public class PercentageDiscount : Rule
-    {
-        private readonly string m_Item;
-        private readonly int m_Percentage;
-
-        public PercentageDiscount(string item, int percentage)
-        {
-            m_Item = item;
-            m_Percentage = percentage;
-        }
-
-        public override Basket Apply(Basket basket)
-        {
-            var discountedBasket = new Basket();
-
-            foreach (var item in basket)
-            {
-                discountedBasket.Add(item);
-
-                if (item.Item.Equals(m_Item) && !item.UsedInOffer)
-                {
-                    var discountToApply =  -(int) Math.Ceiling(item.UnitPrice * ((double) m_Percentage / 100));
-                    discountedBasket.Add(string.Format("{0}:{1}% discount", m_Item, m_Percentage), discountToApply);
-                }
-            }
-
-            return discountedBasket;
         }
     }
 }
