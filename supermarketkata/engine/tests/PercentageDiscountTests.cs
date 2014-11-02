90f4e2245e7043fac8298f826eb4f65d528a2fbd
Luke Drury <lukecdrury@gmail.com>
2014-11-02 16:49:25 +0000

    Test file for PercentageDiscount rule

---
+++ supermarketkata/engine/tests/PercentageDiscountTests.cs
@@ -0,0 +1,17 @@
+﻿using engine.core;
+using NUnit.Framework;
+
+namespace engine.tests
+{
+    [TestFixture]
+    public class PercentageDiscountTests
+    {
+        private Basket m_InputBasket;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_InputBasket = new Basket();
+        }
+    }
+}
