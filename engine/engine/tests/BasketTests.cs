4b74368c58d75b02dfca054dcc2edee77f8b0028
Luke Drury <dakmor00@gmail.com>
2014-11-02 00:24:45 +0000

    Red: failing test for throwing when item added to basket with negative quantity

--- engine/engine/tests/BasketTests.cs
+++ engine/engine/tests/BasketTests.cs
@@ -1,10 +1,16 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace engine.tests
 {
     [TestFixture]
     public class BasketTests
     {
-         
+        [Test]
+        public void ThrowsWhenQuantityIsNegativeNumber()
+        {
+            var basket = new Basket();
+            Assert.Throws<ArgumentOutOfRangeException>(() => basket.Add("item", 1, -1));
+        }
     }
 }
